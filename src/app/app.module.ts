474aeb70708f74a227b8ea8d21dccc7f5c8d41f2
EC2 Default User <ec2-user@ip-172-31-11-48.ec2.internal>
2018-07-16 01:29:03 +0000

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 import { CalendarModule } from 'angular-calendar';
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
     CalendarComponent,
-    HeaderNavComponent
+    HeaderNavComponent,
+    PageNotFoundComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule,
     BrowserAnimationsModule,
     CalendarModule.forRoot()
--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavItem } from '../NavItem';
 
 @Component({
   selector: 'app-header-nav',
@@ -6,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-nav.component.scss']
 })
 export class HeaderNavComponent implements OnInit {
-  navItems: string[] = [
-    "About Me",
-    "Calendar"
+  navItems: NavItem[] = [
+    {label:"Home", url:""},
+    {label:"Calendar", url:"/calendar"}
   ];
   constructor() { }
 
